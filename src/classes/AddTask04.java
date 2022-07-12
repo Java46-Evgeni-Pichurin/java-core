8aea21c21c22bf28306f9d114cd356fb3af90b59
EvgeniPichurin <pichurinaa@gmail.com>
2022-07-12 21:06:23 +0300

    A-04

--- src/classes/AddTask04.java
+++ src/classes/AddTask04.java
@@ -7,18 +7,17 @@ public class AddTask04 {
 
     public static boolean hasaValidBrackets(String text) {
         text = text.replaceAll("[^(){}\\[\\]]", "");
-        StringBuilder str = new StringBuilder();
         do {
             if (text.contains(brackets1)) {
-                text = text.replace(brackets1,"");
+                text = text.replace(brackets1, "");
             }
             if (text.contains(brackets2)) {
-                text = text.replace(brackets2,"");
+                text = text.replace(brackets2, "");
             }
             if (text.contains(brackets3)) {
-                text = text.replace(brackets3,"");
+                text = text.replace(brackets3, "");
             }
-        }while (text.contains(brackets1) || text.contains(brackets2) || text.contains(brackets3));
+        } while (text.contains(brackets1) || text.contains(brackets2) || text.contains(brackets3));
         return text.isEmpty();
     }
 }
