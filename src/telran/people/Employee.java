0c5bfd4197d15562a39302edbd02d07db912aa0a
Evgeni Pichurin <pichurinaa@gmail.com>
2022-07-20 11:47:20 +0300

    HW-10

---
+++ src/telran/people/Employee.java
@@ -0,0 +1,26 @@
+package telran.people;
+
+public class Employee extends Person {
+    private int basicSalary;
+
+    public Employee(long id, int birthYear, String email, int basicSalary) {
+        super(id, birthYear, email);
+        setBasicSalary(basicSalary);
+    }
+
+    public void setBasicSalary(int basicSalary) {
+        this.basicSalary = Math.abs(basicSalary);
+    }
+
+    public int computePay() {
+        return basicSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee {\n\t\t" +
+                super.toString() +
+                "\n\t\tbasicSalary=" + basicSalary +
+                "\t\n }";
+    }
+}
