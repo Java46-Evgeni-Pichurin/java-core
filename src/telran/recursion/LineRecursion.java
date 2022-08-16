59542a6a5ff9d5237fd27447f1a3ab8087f07091
Evgeni Pichurin <pichurinaa@gmail.com>
2022-08-16 12:35:47 +0300

    HW-18

---
+++ src/telran/recursion/LineRecursion.java
@@ -0,0 +1,86 @@
+package telran.recursion;
+
+public class LineRecursion {
+
+    /***************************************Homework*************************************************/
+    /**
+     * @param x number
+     * @return x ^ 2
+     */
+    public static int square(int x) {
+        return getSum(x, x);
+    }
+
+    /**
+     * @param a either negative or positive
+     * @param b positive
+     * @return a ^ b
+     */
+    public static long pow(int a, int b) {
+        boolean isPositiveNumber = a > 0;
+        boolean isOddPow = isOddNumber(b);
+        switch (b) {
+            case 0:
+                return 1;
+            case 1:
+                return a;
+            default:
+                if (!isPositiveNumber && isOddPow) {
+                    a = -a;
+                    return -getPow(a, a, b - 2);
+                }
+                return getPow(a, a, b - 2);
+        }
+    }
+
+    private static boolean isOddNumber(int b) {
+        return switch (b) {
+            case -1 -> true;
+            case 0 -> false;
+            default -> isOddNumber(b - 2);
+        };
+    }
+
+    private static int getPow(int num, int sum, int pow) {
+        sum = getSum(sum, num);
+        if (pow == 0) {
+            return sum;
+        }
+        return getPow(num, sum, pow - 1);
+    }
+
+    private static int getSum(int a, int count) {
+        if (count == 0) {
+            return 0;
+        }
+        if (a < 0) {
+            a = -a;
+            count = -count;
+        }
+        return a + getSum(a, count - 1);
+    }
+
+    /*************************************************************************************************/
+
+    public static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    /**
+     * @param ar - array of in integer numbers
+     * @return sum of all numbers from the given array
+     */
+    public static int sum(int[] ar) {
+        return sum(0, ar);
+    }
+
+    private static int sum(int firstIndex, int[] ar) {
+        if (firstIndex == ar.length) {
+            return 0;
+        }
+        return ar[firstIndex] + sum(firstIndex + 1, ar);
+    }
+}
