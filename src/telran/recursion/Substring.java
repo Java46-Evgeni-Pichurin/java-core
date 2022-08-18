70f040d11f8dfc86ec8e6ec3c3ac76b94b2849f9
Evgeni Pichurin <pichurinaa@gmail.com>
2022-08-18 10:22:03 +0300

    HW-18

--- src/telran/recursion/Substring.java
+++ src/telran/recursion/Substring.java
@@ -13,21 +13,21 @@ public class Substring {
             return true;
         }
         if (str.charAt(0) == substr.charAt(0)) {
-            // move forward both
+            // moving forward both
             return isSubstring(str.substring(1), substr.substring(1), initSubstr);
         }
         if (str.charAt(0) == initSubstr.charAt(0)) {
             // checking equality of the first char of initial substring with the first char of string
-            // move forward both (substring from the initial form)
+            // moving forward both, from the second char (substring from the initial form)
             return isSubstring(str.substring(1), initSubstr.substring(1), initSubstr);
         }
         if (str.charAt(0) == initSubstr.charAt(1)) {
             // checking equality of the second char of initial substring with the first char of string
-            // move forward only string from the second char
+            // moving forward only for string from the third char
             // substring keeps current form
             return isSubstring(str.substring(2), substr, initSubstr);
         }
-        // move forward only string
+        // moving forward only for string from the second char
         // substring comes back to initial form
         return isSubstring(str.substring(1), initSubstr, initSubstr);
     }
