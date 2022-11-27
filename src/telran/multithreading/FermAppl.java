c2c67ddedca0c48655f91286711a0137b6bc1898
Evgeni Pichurin <pichurinaa@gmail.com>
2022-11-27 16:59:55 +0200

    HW-43

--- src/telran/multithreading/FermAppl.java
+++ src/telran/multithreading/FermAppl.java
@@ -9,29 +9,36 @@ public class FermAppl {
 
     private static final int N_TRUCKS = 2000;
     private static final int N_LOADS = 500;
+    private static final int N_RUNS = 10;
 
     public static void main(String[] args) {
+        long avgTime;
         Truck[] trucks = new Truck[N_TRUCKS];
+        System.out.printf("Start trucks with common mutex (%d runs):\n", N_RUNS);
+        avgTime = startRunning(trucks , true);
+        System.out.printf("Average running time is :%d\n", avgTime);
+        System.out.printf("\nStart trucks with different mutexes (%d runs):\n", N_RUNS);
+        avgTime = startRunning(trucks , false);
+        System.out.printf("Average running time is :%d\n", avgTime);
+    }
+
+    private static long startRunning(Truck[] trucks, boolean hasCommonMutex) {
         Instant start = Instant.now();
-        System.out.println("Start trucks with common mutex:\n");
-        startTrucks(trucks, true);
-        waitingForFinishing(trucks);
-        printReport(start);
-        Truck.refreshElevators();
-        System.out.println("Start trucks with different mutexes:\n");
-        start = Instant.now();
-        startTrucks(trucks, false);
-        waitingForFinishing(trucks);
-        printReport(start);
+        long totalRunningTime = 0;
+        for (int i = 0; i < N_RUNS; i++) {
+            startTrucks(trucks, hasCommonMutex);
+            waitingForFinishing(trucks);
+            printReport(start);
+            Truck.refreshElevators();
+            totalRunningTime += ChronoUnit.MILLIS.between(start, Instant.now());
+            start = Instant.now();
+        }
+        return totalRunningTime / N_RUNS;
     }
 
     private static void printReport(Instant start) {
         System.out.printf("""
-                        Report: elevator1 contains %d tons; elevator2 contains %d tons
-                        running time is %d
-                        number of waiting iterations is %d
-                        ===============================================================
-                        
+                        Report: elevator1 contains %d tons; elevator2 contains %d tons; running time is %d; number of waiting iterations is %d
                         """,
                 Truck.getElevator1(), Truck.getElevator2(),
                 ChronoUnit.MILLIS.between(start, Instant.now()), Truck.getWaitingCounter());
@@ -47,10 +54,10 @@ public class FermAppl {
 
     }
 
-    private static void startTrucks(Truck[] trucks, boolean isCommonMutex) {
+    private static void startTrucks(Truck[] trucks, boolean hasCommonMutex) {
         IntStream.range(0, trucks.length)
                 .forEach(i -> {
-                    trucks[i] = new Truck(1, N_LOADS, isCommonMutex);
+                    trucks[i] = new Truck(1, N_LOADS, hasCommonMutex);
                     trucks[i].start();
                 });
     }
