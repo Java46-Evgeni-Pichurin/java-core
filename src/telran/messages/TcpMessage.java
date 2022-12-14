f73638c064d74000e3fa155c36f85ed07423c9a6
Evgeni Pichurin <pichurinaa@gmail.com>
2022-12-14 11:22:03 +0200

    HW-48

--- src/telran/messages/TcpMessage.java
+++ src/telran/messages/TcpMessage.java
@@ -4,7 +4,7 @@ public class TcpMessage implements Message {
     String ipPort; //ipv4:port
 
     public TcpMessage(String ipPort) {
-        if (ipPort.matches("[1-2][0-4]?\\d?.[0-2][0-5]?[0-5]?.[0-2][0-5]?[0-5]?.[0-2][0-5]?[0-5]?:\\d{2}\\d?\\d?\\d?")) {
+        if (ipPort.matches("[1-2][0-4]?\\d?.[0-2][0-5]?[0-5]?.[0-2][0-5]?[0-5]?.[0-2][0-5]?[0-5]?:[1-9]\\d\\d?\\d?\\d?")) {
             this.ipPort = ipPort;
         } else {
             throw new RuntimeException();
